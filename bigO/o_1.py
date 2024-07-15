9155767439004d4b477103e7054ed24b9447e7c0
Milos Roknic <roknic.milos.994@gmail.com>
2024-07-15 17:24:40 +0200

    Add O(1) example

---
+++ bigO/o_1.py
@@ -0,0 +1,9 @@
+len(range(1, 10))
+
+"""
+If we increase the range to 10x more than the previous range,
+the time it takes to calculate the length using len(range(...)) 
+will remain the same.
+
+This is called O(1) or CONSTANT TIME COMPLEXITY.
+"""
