bb5af1c1cce2f1e6be64d0cccedd1468db57e23a
Milos Roknic <roknic.milos.994@gmail.com>
2024-07-15 17:22:54 +0200

    Add O(N) example

---
+++ bigO/o_n.py
@@ -0,0 +1,11 @@
+items = list(range(1, 10))
+
+for item in items:
+    print(item)
+
+"""
+If we increase the range to 10x more that the previous range, 
+the time it takes to run the code will also increase 10x more.
+
+This is called O(n) or LINEAR TIME COMPLEXITY.
+"""
