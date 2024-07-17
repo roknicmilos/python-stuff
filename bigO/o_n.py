7f038f7f0978564dc178424cc163ca32c5aa7d09
Miloš Roknić <roknic.milos.994@gmail.com>
2024-07-17 21:08:39 +0200

    Add 'find mom' example for O(N)

--- bigO/o_n.py
+++ bigO/o_n.py
@@ -8,3 +8,44 @@ proportional to the size of the range.
 
 This is called O(n) or LINEAR TIME COMPLEXITY.
 """
+
+# "Find mom" example:
+
+moms = [
+    ("Alice", "Jane"),
+    ("Sam", "Sarah"),
+    ("Tom", "Sally"),
+    ("Sarah", "Sue"),
+    ("Bob", "Bella"),
+]
+
+
+def find_mom(child):
+    """
+    On average, we're going to go through half
+    of the list (N/2) to find the mom.
+
+    Each loop has 3 steps:
+        1. Get the mom and child names tuple
+        2. Assign the mom to mom_name
+        3. Assign the child to child_name
+
+    `if` statement inside the loop has 1 step - happens
+    once per iteration.
+
+    And finally, the return statement will happen once.
+
+    So, the time complexity of this function is:
+    3 * N/2 + N/2 + 1 = 2N + 1 = O(N)
+    If we get rid of the lower order components (1) and
+    we get rid the coefficient (2), we get O(N).
+
+    Conclusion:
+        find_mom() has a linear time complexity.
+        find_mom() is O(N).
+    """
+
+    for mom_name, child_name in moms:  # 3 * N/2
+        if child == child_name:  # 1 * N/2
+            return mom_name  # 1
+    return None
